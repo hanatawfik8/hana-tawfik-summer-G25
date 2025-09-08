d9631e253dc415c9afd881d1e565b27d1c90e6e8
hanatawfik8 <hannatawfik35@gmail.com>
2025-09-08 13:19:19 +0300

    Looping Task

--- session-12-tasks/js/main.js
+++ session-12-tasks/js/main.js
@@ -9,14 +9,43 @@
 // else console.log("F");
 
 // For Task
-var breakVar = "Hana";
-var input;
-for (var i = 0; i < 5; i++) {
-  input = prompt("Enter a name");
-  if (input == breakVar) {
-    console.log("Breaking...");
-    break;
-  } else {
-    console.log("No Break");
+// var breakVar = "Hana";
+// var input;
+// for (var i = 0; i < 5; i++) {
+//   input = prompt("Enter a name");
+//   if (input == breakVar) {
+//     console.log("Breaking...");
+//     break;
+//   } else {
+//     console.log("No Break");
+//   }
+// }
+
+// Looping Task
+function looping(startNum, endNum, continueNum, breakNum) {
+  if (
+    startNum == undefined ||
+    endNum == undefined ||
+    breakNum == undefined ||
+    continueNum == undefined
+  ) {
+    console.log("Please enter all numbers");
+    return;
+  } else if (startNum > endNum) {
+    console.log("Start Number must be less than End NUmber");
+    return;
+  } else if (breakNum <= continueNum) {
+    console.log("Break Number must be greater than Continue Number");
+    return;
+  }
+
+  for (var i = startNum; i <= endNum; i++) {
+    if (i == continueNum) continue;
+    else if (i == breakNum) break;
+    else console.log(i);
   }
 }
+
+looping(1, 5);
+looping(1, 5, 3);
+looping(1, 5, 2, 4);
